2ea95afdf82d3b7a78a62144ac973c3c8456d4fe
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-02-29 18:00:02 +0300

    add new

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 document.getElementById('yes').onclick = function() {
     var login = document.getElementById('login').value;
     var pass = document.getElementById('pass').value;
-    var yourLogin = 'Данил';
-    var yourPass = 1234;
+    var yourLogin = 'Егор';
+    var yourPass = 66;
 
     if (pass == yourPass && login == yourLogin) {
         document.getElementById('out').innerHTML = 'Подтверждено!';
     };
 
     if (pass != yourPass || login != yourLogin) {
+        document.getElementById('login').value = '';
+        document.getElementById('pass').value = '';
         document.getElementById('out').innerHTML = 'Логин или пароль не верны! Введите ещё раз.';
     };
 };
